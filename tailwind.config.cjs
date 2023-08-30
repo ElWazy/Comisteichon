99aa34e0296e3c885e7a1acf8a24d6413148556f
TamVanum <gaston.dev14@gmail.com>
2023-08-29 22:22:01 -0400

    fix index.astro and navbar.astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'press-start': ['"Press Start 2P"', 'cursive'],
+				'dancing-script': ['"Dancing Script"', 'cursive'],
+				'shadow-into': ['"Shadows Into Light"', 'cursive'],
+
+			},
+			colors: {
+				'regal-blue': '#243c5a',
+			},
+		},
+
 	},
 	plugins: [],
+
 }
+
+
