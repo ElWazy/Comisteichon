c7ff9f105549bad3fcab33bdcae9236790102b8f
ElWazy <ElCazero.Fer@gmail.com>
2024-09-25 00:02:47 -0300

    build: actualizando astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: { assets: true },
   server: {
     host: true,
   },
