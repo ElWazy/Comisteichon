e78512019a170748198ba62b96a18e3cb0a27676
ElWazy <ElCazero.Fer@gmail.com>
2024-09-24 23:13:40 -0300

    feat: hall of fame

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  experimental: { assets: true },
   server: {
     host: true,
   },
